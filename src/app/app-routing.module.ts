322aa61b50d5ecc5093617b4cb5875e9dc74ee03
luanof <luan.o.f@hotmail.com>
2022-12-16 00:50:00 -0300

    fazendo a rota da tela de  oferta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DiversaoComponent } from "./diversao/diversao.component";
 import { HomeComponent } from "./home/home.component";
+import { OfertaComponent } from './oferta/oferta.component';
 import { RestauranteComponent } from "./restaurante/restaurante.component";
 
 
 const routes: Routes = [
   { path: 'Diversao', component: DiversaoComponent },
   { path: '', component: HomeComponent },
-  { path: 'Restaurante', component: RestauranteComponent }
+  { path: 'Restaurante', component: RestauranteComponent },
+  { path: 'oferta/:id', component: OfertaComponent }
 ];
 
 @NgModule({
--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../Core/service/ofertas.service';
+import { Ofertas } from '../Shared/Models/ofertas.model';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
   styleUrls: ['./oferta.component.scss']
 })
-export class OfertaComponent {
+export class OfertaComponent implements OnInit {
+
+  public oferta!: Ofertas;
+
+  constructor(
+    private ofertasService: OfertasService,
+    private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    const id = Number(this.route.snapshot.params['id']);
+    this.ofertasService.getofertasId(id).then((ofertas: Ofertas) => this.oferta = ofertas);
+
+  }
+
+
+
 
 }
